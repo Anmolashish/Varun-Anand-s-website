47f43a48f03af46a1a1fc8932b10bbad87495380
aniketsharma953 <sharmaniket593@gmail.com>
2024-09-16 17:07:37 +0530

    addaniketwork

--- src/components/Homepage2.jsx
+++ src/components/Homepage2.jsx
@@ -1,3 +1,55 @@
 export default function Homepage2() {
-  return <div></div>;
+  return (
+    <div className="home-page2">
+      <div id="pat-heading">
+        <h1>हमारे साथी कलाकार</h1>
+      </div>
+      <div id="pattners">
+        <div>
+          <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
+        </div>
+        <div>
+          <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
+        </div>
+        <div>
+          <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
+        </div>
+        <div>
+          <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
+        </div>
+        <div>
+          <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
+        </div>
+        <div>
+          <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
+        </div>
+      </div>
+      <div id="pat-heading">
+        <h1>समीक्षा</h1>
+      </div>
+      <div class="review">
+        <div class="review-head">
+          <h2>amazing shayari it feels magic</h2>
+          <div>
+            <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+          </div>
+          <h4>-Mrunal Thakur</h4>
+        </div>
+        <div class="review-head">
+          <h2>amazing shayari it feels magic</h2>
+          <div>
+            <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+          </div>
+          <h4>-Mrunal Thakur</h4>
+        </div>
+        <div class="review-head">
+          <h2>amazing shayari it feels magic</h2>
+          <div>
+            <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+          </div>
+          <h4>-Mrunal Thakur</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
