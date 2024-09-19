1e629108c64d17e6d91e00103fd3852465059d21
aniketsharma953 <sharmaniket593@gmail.com>
2024-09-19 12:36:19 +0530

    Fixed some text issue

--- src/components/Homepage2.jsx
+++ src/components/Homepage2.jsx
@@ -24,30 +24,32 @@ export default function Homepage2() {
           <img src="Images/fellow.jpeg" id="pat-img" loading="lazy" />
         </div>
       </div>
-      <div id="pat-heading">
-        <h1>समीक्षा</h1>
-      </div>
-      <div class="review">
-        <div class="review-head">
-          <h2>amazing shayari it feels magic</h2>
-          <div>
-            <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
-          </div>
-          <h4>-Mrunal Thakur</h4>
+      <div className="review-container">
+        <div id="pat-heading">
+          <h1> समीक्षा</h1>
         </div>
-        <div class="review-head">
-          <h2>amazing shayari it feels magic</h2>
-          <div>
-            <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+        <div class="review">
+          <div class="review-head">
+            <h2>amazing shayari it feels magic</h2>
+            <div>
+              <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+            </div>
+            <h4>-Mrunal Thakur</h4>
           </div>
-          <h4>-Mrunal Thakur</h4>
-        </div>
-        <div class="review-head">
-          <h2>amazing shayari it feels magic</h2>
-          <div>
-            <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+          <div class="review-head">
+            <h2>amazing shayari it feels magic</h2>
+            <div>
+              <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+            </div>
+            <h4>-Mrunal Thakur</h4>
           </div>
-          <h4>-Mrunal Thakur</h4>
+          <div class="review-head">
+            <h2>amazing shayari it feels magic</h2>
+            <div>
+              <img src="Images/review.jpeg" id="rev-img" loading="lazy" />
+            </div>
+            <h4>-Mrunal Thakur</h4>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Homepage5.jsx
@@ -0,0 +1,27 @@
+export default function Homepage2() {
+  return (
+    <div className="home-page5">
+      <div class="Event">
+        <center>
+          <h1>आयोजन</h1>
+        </center>
+        <div class="event-head">
+          <div id="eve-card">
+            <img src="Images/review.jpeg" id="eve-img" loading="lazy" />
+            <h2>shayari Mahaotsav in Jalandhar</h2>
+            <h2>2024-8-19</h2>
+            <p>
+              Biggest shayari event organized in
+              <br />
+              jalandhar punjab
+            </p>
+            <section className="contact-button">
+              {" "}
+              <div className="button">Read More</div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
