bc66e1056ecb54d5caec9013b35d37f41eea788c
Anmol aashish <aabatwal20@gmail.com>
2024-10-02 14:12:22 +0530

    Added images and made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import "./App.css";
-import Homepage1 from "./components/Homepage1";
-import Homepage2 from "./components/Homepage2";
-import Homepage3 from "./components/Homepage3";
-import Homepage4 from "./components/Homepage4";
-import Homepage5 from "./components/Homepage5";
+// import Home from "./components/Home";
+
 import Navbar from "./components/Navbar";
-import Numbers from "./components/Numbers";
-// import Slideshow from "./components/slideshow";
-import TextTape from "./components/TextTape";
+import PoemSection from "./components/PoemSection";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Homepage1 />
-      <Homepage2 />
-      <Homepage3 />
-      <TextTape />
-      {/* <Slideshow /> */}
-      <Homepage4 />
-      <Numbers />
-      <Homepage5 />
+      {/* <Home /> */}
+      <PoemSection />
     </>
   );
 }
