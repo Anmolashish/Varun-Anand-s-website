2ad955a75f12e45b6bf1a5d82d29d5888d156da0
Anmol aashish <aabatwal20@gmail.com>
2025-01-24 11:30:15 +0530

    Added sitemap options

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.varunanand.in", // Replace with your website URL
+  generateRobotsTxt: true, // Automatically generate a robots.txt file
+  sitemapSize: 7000, // Optional: Limit URLs per sitemap file (if needed)
+};
